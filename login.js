85bf44f1f739256c55780a4f6f568f598708e467
Collins Ikpeyi <Ikpeyi89@gmail.com>
2022-04-06 16:48:47 -0500

    new Commit

---
+++ login.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('#Sign-btn');
+let users = "Collins@gmail.com";
+let pass = "password"
+
+
+
+button.addEventListener('click',function(event){
+    const userName = document.querySelector('#userName').value;
+    const passWord = document.querySelector('#passWord').value;
+    var link = document.querySelector("a");
+    console.log(userName, passWord);
+    if(userName!=users || pass != passWord){
+        alert("Incorrect Password or Email");
+        link.setAttribute("href",
+        "login.html");
+        
+    } else {
+        link.setAttribute("href",
+        "index.html");
+        console.log("working");
+    }
+
+})
+

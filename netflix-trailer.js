cabf0a72e8be3e3078afd9386b72ee86e382a193
jaylawms19 <70493251+jaylawms19@users.noreply.github.com>
2022-04-06 23:52:02 -0500

    Add files via upload
    Clicking a movie genre corresponds to a trailer slideshow page showing movies related to the genre.

--- netflix-trailer.js
+++ netflix-trailer.js
@@ -1,3 +1,9 @@
+// npm install --save moduleName
+// import { createRequire } from 'rotten-tomatoes';
+// const require = createRequire(import.meta.url);
+
+console.log("reading js file");
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +32,26 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// var tomatoes = require('rotten-tomatoes');
+// var movies = tomatoes('6nkt9qb3ggxbd3ejyzsjvq3x');
+
+// movies.search('Ninja Turtles', function(err, results) {
+//   results.sort(function(a, b) {
+//     return b.ratings.audience_score - a.ratings.audience_score;
+//   });
+//   console.log('The best Ninja Turtles movies of all time');
+//   console.log('=========================================');
+//   results.forEach(function(movie) {
+//     console.log(movie.ratings.audience_score + '\t' + movie.title + ' (' + movie.year + ')');
+//   });
+// });
+
+
+// const button = document.querySelector('#btn-rate');
+// button.addEventListener('click',function(event){
+//   // const rating = Number(document.querySelector('#rating').value);
+//   console.log("button clicked");
+//   const resultElement = document.querySelector("#result");
+//   resultElement.innerHTML = "5";
+// });
